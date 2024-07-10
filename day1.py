2e4c5fff4983e650c933085c3fee3510f741b660
HARSH TIWARI <139095111+harsh78552@users.noreply.github.com>
2024-07-10 15:38:43 +0530

    Let us python coding question practice

---
+++ day1.py
@@ -0,0 +1,10 @@
+Input1 = int(input("Enter your number: "))
+Input2 = int(input("Enter your second number: "))
+print(f"{Input1} + {Input2} = {Input1 + Input2}")
+print(f"{Input1} - {Input2} = {Input1 - Input2}")
+print(f"{Input1} * {Input2} = {Input1 * Input2}")
+print(f"{Input1} / {Input2} = {Input1 / Input2}")
+print(f"{Input1} % {Input2} = {Input1 % Input2}")
+print(f"{Input1} // {Input2} = {Input1 // Input2}")
+print(f"{Input1} ** {Input2} = {Input1 ** Input2}")
+
