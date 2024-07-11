a9ccf754fa90956ec8ea2683db609dc9d0b456bb
HARSH TIWARI <139095111+harsh78552@users.noreply.github.com>
2024-07-11 18:12:36 +0530

    Next another day of coding journey.
    In this file i solve the coding question of "Let Us Python Book".

---
+++ Coding_Ch_04.py
@@ -0,0 +1,33 @@
+"""String = 'Shenanigan'
+String1 = 'Type'
+String2 = 'Wabbit'
+print(String[:2])
+print(String[4:6])
+print(String[2:])
+print(String[:6])
+print(String[:10])
+print(String[:10:2])
+print(String[:10:3])
+print(String[:10:4])
+print(String + String1)
+print(String + String2)"""
+
+"""String = 'Visit ykanetkar.com for online courses in programming.'
+print(String.title())"""
+
+"""String = 'Light travels faster than sound.This is why some people appear bright until you hear them speak.'
+print(String.replace('Light', 'LIGHT').replace('sound', 'SOUND'))"""
+
+"""String = 'The difference between stupidity and genius is that genius has its limits.'
+print(String.split(" "))"""
+
+# Variable = " He said, \"Let Us python\" "
+# print(Variable)
+# Variable1 = """ He said, "Let Us python" """
+# print(Variable1)
+
+"""s3 = 'C:\\Users\\Kanetkar\\Documents'
+print(s3.split('\\'))
+print(s3.partition('\\'))"""
+
+
