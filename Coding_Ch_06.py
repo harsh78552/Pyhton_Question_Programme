031d7ffc66ef59dea95e8e6b9d24e9f257aa7f43
HARSH TIWARI <139095111+harsh78552@users.noreply.github.com>
2024-07-14 20:16:40 +0530

    increment another one day

---
+++ Coding_Ch_06.py
@@ -0,0 +1,25 @@
+"""for i in range(1, 11):
+    print(i)"""
+
+"""for i in range(20, 10, -3):
+    print(i, end=" ")"""
+
+"""j = 1
+while j <= 10:
+    print(j)
+    j += 1"""
+
+"""lst = [10, 20, 30, 40, 50]
+Len = len(lst)
+for index in range(0, Len):
+    print(lst[index])"""
+
+"""count = 0
+for i in range(1, 101):
+    if i % 2 != 0:
+        print(i)
+        count += 1
+    if count == 25:
+        break"""
+
+
