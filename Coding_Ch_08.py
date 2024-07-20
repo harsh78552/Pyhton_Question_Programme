876d7c42236f9462b97f38cc1313f8d23742b39e
HARSH TIWARI <139095111+harsh78552@users.noreply.github.com>
2024-07-20 16:22:39 +0530

    Let_Us_Python_List_P

--- Coding_Ch_08.py
+++ Coding_Ch_08.py
@@ -34,7 +34,129 @@ print(type(lst))
 print(type(lst2))
 print(type(lst3))"""
 
-num1 = [10, 25, 4, 122, 3, 8]
-temp = num1[0]
-for i in num1:
-    
+"""lst = [-1, 0, 1, 2, 8, 3, 4, 5, 6]
+for i in range(len(lst)):
+    max_index = i
+    for j in range(i + 1, len(lst)):
+        if lst[j] < lst[max_index]:
+            max_index = j
+    lst[i], lst[max_index] = lst[max_index], lst[i]
+print(lst)"""
+
+"""lst = [10, 25, 4, 12, 3, 8,30]
+num = 30
+Bool = False
+for i in lst:
+    if i == num:
+        Bool = True
+        break
+if Bool:
+    print("Match")
+else:
+    print("Not match")"""
+
+"""lst = [10, 25, 4, 12, 3, 8]
+lst.insert(2,30)
+print(lst)"""
+
+"""S = "Hello"
+lst=[]
+for i in S:
+    lst.append(i)
+print(lst)"""
+
+"""Num = int(input("Enter your number:"))
+lst = []
+lst1 = []
+for i in range(0, Num):
+    if i % 2 != 0:
+        lst.append(i)
+    if len(lst) > 3:
+        break
+
+for j in range(1, 100):
+    if j % 2 == 0:
+        lst1.append(j)
+    if len(lst1) > 3:
+        break
+lst[3] = lst1"""
+
+"""import random
+
+lst = []
+Lst1 = []
+for i in range(0, 20):
+    Random = random.randint(1, 100)
+    lst.append(Random)
+print(lst)
+Num = int(input("Enter your number:"))
+for j in range(len(lst)):
+    if lst[j] == Num:
+        Lst1.append(j)
+print(f"Position of {Num} in lst is {Lst1}.")"""
+
+"""lst = [10, 10, 20, 20, 40, 10, 78, 45]
+lst1 = []
+for i in range(1, len(lst)):
+    if lst[i] not in lst1:
+        lst1.append(lst[i])
+print(lst1)"""
+
+"""lst = [1, -9, -4, 2, 3, 4, 5, 6, -1, -3, -5, -8, -9]
+lst1 = []
+lst2 = []
+for i in range(len(lst)):
+    if 0 <= lst[i] <= 9:
+        lst1.append(lst[i])
+    else:
+        lst2.append(lst[i])
+print(lst1)
+print(lst2)"""
+
+"""lst = ['Aman', 'Harsh', 'Adarsh', 'Pushkar']
+lst1 = [i.upper() for i in lst]
+print(lst1)"""
+
+"""temperature_fahrenheit = [32, 68, 72, 98.6, 212, -40, -20, 100, 120, 100.4]
+temperature_celcius = []
+for i in temperature_fahrenheit:
+    temperature_celcius.append(round((5 / 9) * (i - 32), 3))
+print(temperature_celcius)"""
+
+"""List = [3, 1, 4, 2, 5, 6, 122, 8, 7, 9, 5, 6]
+for i in range(len(List)):
+    Min_index = i
+    for j in range(i + 1, len(List)):
+        if List[j] > List[Min_index]:
+            Min_index = j
+        List[i], List[Min_index] = List[Min_index], List[i]
+print(List)
+Len_List = len(List)
+if Len_List % 2 != 0:
+    Divide = Len_List // 2
+    print(List[Divide])
+else:
+    Divide1 = Len_List // 2
+    Divide2 = Divide1 - 1
+    Median = (List[Divide1] + List[Divide2]) / 2
+    print(Median)"""
+
+"""Lst = [-1, -1, -1, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6, 7]
+Count = 0
+for Num in range(0, len(Lst)):
+    if Lst[Num] < 0:
+        Count += 1
+print(f"Number of negative integer in List is {Count}.")"""
+
+"""Lst = ['Harsh', 'Adarsh', 'Pushkar', 'Aryan']
+Lst1 = []
+for i in range(0, len(Lst)):
+    Lst1.append(Lst[i][0])
+print(Lst1)"""
+
+"""Lst1 = [10, 20, 30, 40, 50, 20, 30, 40]
+Lst = []
+for i in range(0, len(Lst1)):
+    if Lst1[i] not in Lst:
+        Lst.append(Lst1[i])
+print(Lst)"""
